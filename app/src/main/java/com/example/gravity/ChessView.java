24ec3d99a50b863e086cfef28b7fd216692ecc62
Hal Smith <hal@schnibb.com>
2020-04-13 01:05:50 +0100

    Select square works on all squares. Need to make it only select squares with pieces on.

--- app/src/main/java/com/example/gravity/ChessView.java
+++ app/src/main/java/com/example/gravity/ChessView.java
@@ -27,18 +27,20 @@ public class ChessView extends View {
     boolean touched = false;
     float xTouch, yTouch;
     private Rect selectRect = new Rect();
+    private ChessSquare selectedSquare;
 
     public ChessView(Context context) {
         super(context);
-        mRect.set(1000, 1000, 1000, 1000);
-        mPaint.setColor(getResources().getColor(R.color.chessBrown));
-        mPaint.setStrokeWidth(BORDER_WIDTH);
-        mPaint.setStyle(Paint.Style.STROKE);
+
         totodileBackground = BitmapFactory.decodeResource(getResources(), R.drawable.totodile);
     }
     @Override
     protected void onDraw(Canvas canvas) {
-        //canvas.drawBitmap(totodileBackground, 0, 0, null);
+        canvas.drawBitmap(totodileBackground, 0, 0, null);
+        mRect.set(1000, 1000, 1000, 1000);
+        mPaint.setColor(getResources().getColor(R.color.chessBrown));
+        mPaint.setStrokeWidth(BORDER_WIDTH);
+        mPaint.setStyle(Paint.Style.STROKE);
         int width = getWidth();
         int height = getHeight();
         int boardSize = width - 2*OFFSET - 2*BORDER_WIDTH;
@@ -48,20 +50,23 @@ public class ChessView extends View {
         boolean fillSquare = true;
         for (int i = 0; i < 8 ; i++){
             for (int j = 0; j < 8; j++){
+                int squareLeft = OFFSET + BORDER_WIDTH + i*squareSize;
+                int squareRight = OFFSET + BORDER_WIDTH + (i + 1)*squareSize;
+                int squareTop = OFFSET + BORDER_WIDTH + j*squareSize;
+                int squareBottom = OFFSET + BORDER_WIDTH + (j + 1)*squareSize;
+
+                squareBounds = new SquareBounds(squareLeft, squareTop, squareRight, squareBottom);
+                squareBoardLocation = getSquareBoardLocation(i, j);
+                mSquare = new ChessSquare(squareBounds, ChessPieceId.NoPiece, squareBoardLocation);
+                allChessSquares.add(mSquare);
                 if (fillSquare){
-                    int squareLeft = OFFSET + BORDER_WIDTH + i*squareSize;
-                    int squareRight = OFFSET + BORDER_WIDTH + (i + 1)*squareSize;
-                    int squareTop = OFFSET + BORDER_WIDTH + j*squareSize;
-                    int squareBottom = OFFSET + BORDER_WIDTH + (j + 1)*squareSize;
+
                     mRect.set(squareLeft, squareTop, squareRight, squareBottom);
                     mPaint.setStyle(Paint.Style.FILL);
                     mPaint.setColor(getResources().getColor(R.color.chessBrown));
                     mPaint.setAlpha(140);
                     canvas.drawRect(mRect, mPaint);
-                    squareBounds = new SquareBounds(squareLeft, squareTop, squareRight, squareBottom);
-                    squareBoardLocation = getSquareBoardLocation(i, j);
-                    mSquare = new ChessSquare(squareBounds, ChessPieceId.NoPiece, squareBoardLocation);
-                    allChessSquares.add(mSquare);
+
                     fillSquare = false;
                 } else {
                     fillSquare = true;
@@ -71,19 +76,10 @@ public class ChessView extends View {
         }
 
         if (touched) {
-            for (ChessSquare chessSquare : ChessView.allChessSquares) {
-                SquareBounds test = chessSquare.getBoundary();
-                if (chessSquare.getBoundary().chessSquareContainsCoordinates(chessSquare.getBoundary(), xTouch, yTouch)){
-                    //Rect rectChessHighlight = new Rect();
-                    //Rect rectChessHighlight = new Rect();
-                    mRect.set(chessSquare.getBoundary().getLeft(), chessSquare.getBoundary().getTop(), chessSquare.getBoundary().getRight(), chessSquare.getBoundary().getBottom());
-                    //Paint highlightPaint = new Paint();
-                    mPaint.setStyle(Paint.Style.FILL);
-                    mPaint.setColor(getResources().getColor(R.color.selectedSquareColour));
-                    //highlightPaint.setAlpha(40);
-                    canvas.drawRect(mRect, mPaint);
-                }
-            }
+            mRect.set(selectedSquare.getBoundary().getLeft(), selectedSquare.getBoundary().getTop(), selectedSquare.getBoundary().getRight(), selectedSquare.getBoundary().getBottom());
+            mPaint.setStyle(Paint.Style.FILL);
+            mPaint.setColor(getResources().getColor(R.color.selectedSquareColour));
+            canvas.drawRect(mRect, mPaint);
         }
     }
 
@@ -94,8 +90,8 @@ public class ChessView extends View {
             xTouch = event.getX();
             yTouch = event.getY();
             for (ChessSquare chessSquare : ChessView.allChessSquares) {
-                //SquareBounds test = chessSquare.getBoundary();
                 if (chessSquare.getBoundary().chessSquareContainsCoordinates(chessSquare.getBoundary(), xTouch, yTouch)){
+                    selectedSquare = chessSquare;
                     touched = true;
                     invalidate();
                 }
@@ -136,10 +132,4 @@ public class ChessView extends View {
         }
         return loc;
     }
-
-    public List<ChessSquare> getAllChessSquares() {
-        return allChessSquares;
-    }
-
-    //private
 }
